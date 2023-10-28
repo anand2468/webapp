fe4d1db363f71c7713190052b8e4d96e1f626b36
Anand Pasupuleti <pasupuletimangapathi@gmail.com>
2023-10-28 22:36:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 from flask import Flask, render_template,request, redirect
+import os
 from fileinput import filename
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)
+
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    files = os.listdir()
+    return render_template("index.html", files = files)
 
 @app.route('/upload', methods=["POST"])
 def upload():
     f = request.files['file']
-    print(f, f.filename)
     try:
-        f.save(f.filename)
-        return f"<h1> uploaded</h1> {f}"
+        f.save("files/{}".format(f.filename))
+        return f"<h1> uploaded</h1> <a href='/'>go back </a>{f}"
     except:
         return redirect('/')
+    
+
+@app.route('/fileviewer', defaults={'folder': 'home'})
+@app.route('/fileviewer/<folder>')
+def viewer(folder):
+    if folder == 'home':
+        files=os.listdir()
+    else:
+        files=os.listdir(folder)
+    return render_template('fileviewer.html',files=files)
+
+@app.route('/demo', defaults={'wd': os.getcwd()+'/files'})
+@app.route('/demo/<wd>')
+def demo(wd):
+    files,folders = list(), list()
+    print(wd)
+    for dirpath, dirnames, filenames in os.walk(wd):
+        folders.append(dirnames)
+        files.append(filenames)
+    return render_template('fileviewer.html',files=files,folders = folders)
 
 if __name__ == '__main__':
-    #app.run(debug=True, host="0.0.0.0", port=8888)
-    app.run()
+    app.run(debug=True,host='0.0.0.0',port=8888)
+    #, port=80, host='0.0.0.0'
